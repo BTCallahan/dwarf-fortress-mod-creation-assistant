d6e960eea2a0cf11d1368fee462b4a9538f914da
BTCallahan <bt.callahan5@gmail.com>
2022-05-22 15:01:58 -0400

    Update global functions to be more user friendly

--- assets/globalFunctions.js
+++ assets/globalFunctions.js
@@ -30,87 +30,182 @@ var errorObject = {
         return this.errorMessages.join("\n");
     }
 };
+function clearPastResults(elementName) {
+    errorObject.errorThrown = false;
+    errorObject.errorMessages = newEmptyStringArray();
+    pushObject.pushTo = newEmptyStringArray();
+    let spaces = document.getElementById(elementName);
+    let numberOfSpaces = spaces.value;
+    pushObject.tabObject = (spaces.value == "0" ? "\t" : " ".repeat(numberOfSpaces));
+}
+function printResults() {
+    let warning = document.getElementById("warnings");
+    warning.innerText = (errorObject.errorThrown ? errorObject.errorMessages.join("\n") : "");
+    let div = document.getElementById("result");
+    div.innerText = pushObject.pushTo.join("");
+}
+var PrefixValueType;
+(function (PrefixValueType) {
+    PrefixValueType[PrefixValueType["id"] = 0] = "id";
+    PrefixValueType[PrefixValueType["name"] = 1] = "name";
+    PrefixValueType[PrefixValueType["class"] = 2] = "class";
+    PrefixValueType[PrefixValueType["value"] = 3] = "value";
+})(PrefixValueType || (PrefixValueType = {}));
 function valueIsValid(value) {
     return (value !== undefined && value !== null);
 }
-function createNumberInput(input) {
+function createNumberInput({ defaultValue, inputMin, inputMax, elementName, elementID, elementClass, titleText }) {
     let conArea = document.createElement("input");
     conArea.type = "number";
-    if (valueIsValid(input.inputMin)) {
-        conArea.min = input.inputMin;
+    if (valueIsValid(inputMin)) {
+        conArea.min = inputMin;
     }
-    if (valueIsValid(input.defaultValue)) {
-        conArea.value = input.defaultValue;
+    if (valueIsValid(defaultValue)) {
+        conArea.value = defaultValue;
     }
-    if (valueIsValid(input.inputMax)) {
-        conArea.max = input.inputMax;
+    if (valueIsValid(inputMax)) {
+        conArea.max = inputMax;
     }
-    if (valueIsValid(input.elementName)) {
-        conArea.name = input.elementName;
+    if (valueIsValid(elementName)) {
+        conArea.name = elementName;
     }
-    if (valueIsValid(input.elementID)) {
-        conArea.id = input.elementID;
+    if (valueIsValid(elementID)) {
+        conArea.id = elementID;
     }
-    if (valueIsValid(input.elementClass)) {
-        conArea.className = input.elementClass;
+    if (valueIsValid(elementClass)) {
+        conArea.className = elementClass;
+    }
+    if (valueIsValid(titleText)) {
+        conArea.title = titleText;
     }
     conArea.step = "1";
     return conArea;
 }
-function createTextInput(input) {
+function createTextInput({ defaultValue, elementName, elementID, elementClass, titleText, maxLength }) {
     let textInput = document.createElement("input");
     textInput.type = "text";
-    if (valueIsValid(input.defaultValue)) {
-        textInput.value = input.defaultValue;
+    if (valueIsValid(defaultValue)) {
+        textInput.value = defaultValue;
     }
-    if (valueIsValid(input.elementName)) {
-        textInput.name = input.elementName;
+    if (valueIsValid(elementName)) {
+        textInput.name = elementName;
     }
-    if (valueIsValid(input.elementID)) {
-        textInput.id = input.elementID;
+    if (valueIsValid(elementID)) {
+        textInput.id = elementID;
     }
-    if (valueIsValid(input.elementClass)) {
-        textInput.className = input.elementClass;
+    if (valueIsValid(elementClass)) {
+        textInput.className = elementClass;
+    }
+    if (valueIsValid(titleText)) {
+        textInput.title = titleText;
+    }
+    if (valueIsValid(maxLength)) {
+        textInput.maxLength = maxLength;
     }
     return textInput;
 }
-function createCheckbox(input) {
+function createCheckbox({ value, isActive = false, elementName, elementId, elementClass, titleText }) {
     let ckbox = document.createElement("input");
     ckbox.type = "checkbox";
-    if (valueIsValid(input.value)) {
-        ckbox.value = input.value;
+    if (valueIsValid(value)) {
+        ckbox.value = value;
     }
-    if (valueIsValid(input.elementName)) {
-        ckbox.name = input.elementName;
+    if (valueIsValid(elementId)) {
+        ckbox.id = elementId;
     }
-    if (valueIsValid(input.elementClass)) {
-        ckbox.className = input.elementClass;
+    if (valueIsValid(elementName)) {
+        ckbox.name = elementName;
     }
-    ckbox.checked = input.isActive;
+    if (valueIsValid(elementClass)) {
+        ckbox.className = elementClass;
+    }
+    if (valueIsValid(titleText)) {
+        ckbox.title = titleText;
+    }
+    ckbox.checked = isActive;
     return ckbox;
 }
-function createSelectOption(value, text) {
+function createSelect({ elementName, elementID, elementClass, titleText }) {
+    let select = document.createElement("select");
+    if (valueIsValid(elementID)) {
+        select.id = elementID;
+    }
+    if (valueIsValid(elementName)) {
+        select.name = elementName;
+    }
+    if (valueIsValid(elementClass)) {
+        select.className = elementClass;
+    }
+    if (valueIsValid(titleText)) {
+        select.title = titleText;
+    }
+    return select;
+}
+function createSelectOption({ value, text }) {
     let blunt = document.createElement("option");
     blunt.value = value;
     blunt.innerText = text;
     return blunt;
 }
-function enableOrDisableElements(input) {
-    let ranged = document.getElementsByClassName(input.className);
+function addOptionToSelect({ value, text, select }) {
+    let blunt = document.createElement("option");
+    blunt.value = value;
+    blunt.innerText = text;
+    select.appendChild(blunt);
+}
+function createFieldset({ fieldsetId, legendText }) {
+    let fieldset = document.createElement("fieldset");
+    fieldset.id = fieldsetId;
+    if (valueIsValid(legendText)) {
+        let legend = document.createElement("legend");
+        legend.innerText = legendText;
+        fieldset.appendChild(legend);
+    }
+    return fieldset;
+}
+function createLabel({ targetId, labelText }) {
+    let label = document.createElement("label");
+    label.htmlFor = targetId;
+    label.innerText = labelText;
+    return label;
+}
+function createParagraph({ elementId, elementsToAppend, elementToAppendParagraphTo }) {
+    let p = document.createElement("p");
+    if (valueIsValid(elementId)) {
+        p.id = elementId;
+    }
+    if (valueIsValid(elementsToAppend)) {
+        elementsToAppend === null || elementsToAppend === void 0 ? void 0 : elementsToAppend.forEach((element) => {
+            p.appendChild(element);
+        });
+    }
+    if (valueIsValid(elementToAppendParagraphTo)) {
+        elementToAppendParagraphTo === null || elementToAppendParagraphTo === void 0 ? void 0 : elementToAppendParagraphTo.appendChild(p);
+    }
+    return p;
+}
+function enableOrDisableElements({ disable, className }) {
+    let ranged = document.getElementsByClassName(className);
     let ranged2 = Array.from(ranged);
     console.log(ranged2);
     ranged2.forEach(element => {
-        element.disabled = input.disable;
+        element.disabled = disable;
     });
 }
 function hideOrUnhideElements({ hideClassNames, unhideClassNames }) {
     hideClassNames.forEach(element => {
         let ele = document.getElementsByClassName(element);
-        ele.hidden = true;
+        let elea = Array.from(ele);
+        elea.forEach((e) => {
+            e.hidden = true;
+        });
     });
     unhideClassNames.forEach(element => {
         let ele = document.getElementsByClassName(element);
-        ele.hidden = false;
+        let elea = Array.from(ele);
+        elea.forEach((e) => {
+            e.hidden = true;
+        });
     });
 }
 function getInputElementValue(inputId) {
@@ -121,17 +216,34 @@ function getSelectElementValue(inputId) {
     let input = document.getElementById(inputId);
     return input.value;
 }
-function getSingleInput({ inputId, numberOfTabObjects, ignoreIfDisabled = true }) {
+function getSingleInput({ inputId, numberOfTabObjects, ignoreIfDisabled = true, ignoreIfBlank, prefixType = PrefixValueType.id }) {
     let input_ = document.getElementById(inputId);
-    if (!ignoreIfDisabled || !input_.disabled) {
-        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", inputId, ":", input_.value, "]\n");
+    if ((!ignoreIfDisabled || !input_.disabled) && (!ignoreIfBlank || input_.value !== "")) {
+        let prefix;
+        if (prefixType === PrefixValueType.id) {
+            prefix = input_.id;
+        }
+        else {
+            prefix = (prefixType === PrefixValueType.name ? input_.name : input_.className);
+        }
+        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", prefix, ":", input_.value, "]\n");
     }
     return input_;
 }
-function getSingleCheckBox({ inputId, numberOfTabObjects, ignoreIfDisabled = true }) {
+function getSingleCheckBox({ inputId, numberOfTabObjects, ignoreIfDisabled = true, prefixType = PrefixValueType.id }) {
     let input_ = document.getElementById(inputId);
     if ((!ignoreIfDisabled || !input_.disabled) && input_.checked) {
-        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", inputId, ":", input_.value, "]\n");
+        let prefix;
+        if (prefixType === PrefixValueType.id) {
+            prefix = input_.id;
+        }
+        else if (prefixType === PrefixValueType.value) {
+            prefix = input_.value;
+        }
+        else {
+            prefix = (prefixType === PrefixValueType.name ? input_.name : input_.className);
+        }
+        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", prefix, "]\n");
     }
     return input_.checked;
 }
@@ -150,7 +262,8 @@ function getMultipleInputs({ inputIds, inputClass, useClassInPlaceOfId = false, 
             throw new ReferenceError("The value for input.inputIds is undefined");
         }
         inputIds.forEach(element => {
-            classElements.push(document.getElementById(element));
+            let el = document.getElementById(element);
+            classElements.push(el);
         });
     }
     if (ignoreIfDisabled) {
@@ -217,9 +330,9 @@ function isCheckboxChecked(inputId) {
     let input_ = document.getElementById(inputId);
     return input_.checked;
 }
-function uncheckCheckbox(input) {
-    if (input.uncheckTarget) {
-        let target = document.getElementById(input.targetId);
+function uncheckCheckbox({ uncheckTarget, targetId }) {
+    if (uncheckTarget) {
+        let target = document.getElementById(targetId);
         target.checked = false;
     }
 }
--- build/globalFunctions.ts
+++ build/globalFunctions.ts
@@ -45,95 +45,178 @@ var errorObject = {
     }
 }
 
+function clearPastResults(elementName:string) 
+{
+    errorObject.errorThrown = false;
+    errorObject.errorMessages = newEmptyStringArray();
+    pushObject.pushTo = newEmptyStringArray();
+    let spaces = <HTMLInputElement><any> document.getElementById(elementName);
+
+    let numberOfSpaces = ((spaces.value as unknown) as number);
+
+    pushObject.tabObject = (spaces.value == "0" ? "\t" : " ".repeat(numberOfSpaces));
+}
+
+function printResults()
+{
+    let warning = <HTMLParagraphElement><any> document.getElementById("warnings");
+
+    warning.innerText = (errorObject.errorThrown ? errorObject.errorMessages.join("\n") : "");
+
+    let div = <HTMLParagraphElement><any> document.getElementById("result");
+
+    div.innerText = pushObject.pushTo.join("");
+}
+
+enum PrefixValueType{
+    id,
+    name,
+    class,
+    value
+}
+
 function valueIsValid(value:any)
 {
     return (value !== undefined && value !== null)
 }
 
-function createNumberInput(input:{defaultValue?:string, inputMin?:string, inputMax?:string, elementName?:string, elementID?:string, elementClass?:string})
+function createNumberInput({
+    defaultValue, inputMin, inputMax, elementName, elementID, elementClass, titleText}:{
+        defaultValue?:string, inputMin?:string, inputMax?:string, elementName?:string, elementID?:string, elementClass?:string, titleText?:string
+    })
 {
     let conArea = document.createElement("input");
 
     conArea.type = "number";
 
-    if (valueIsValid(input.inputMin))
+    if (valueIsValid(inputMin))
     {
-        conArea.min = input.inputMin as string;
+        conArea.min = inputMin as string;
     }
-    if (valueIsValid (input.defaultValue))
+    if (valueIsValid (defaultValue))
     {
-        conArea.value = input.defaultValue as string;
+        conArea.value = defaultValue as string;
     }
-    if (valueIsValid(input.inputMax))
+    if (valueIsValid(inputMax))
     {
-        conArea.max = input.inputMax as string;
+        conArea.max = inputMax as string;
     }
-    if (valueIsValid(input.elementName))
+    if (valueIsValid(elementName))
     {
-        conArea.name = input.elementName as string;
+        conArea.name = elementName as string;
     }
-    if (valueIsValid(input.elementID))
+    if (valueIsValid(elementID))
     {
-        conArea.id = input.elementID as string;
+        conArea.id = elementID as string;
     }
-    if (valueIsValid(input.elementClass))
+    if (valueIsValid(elementClass))
     {
-        conArea.className = input.elementClass as string;
+        conArea.className = elementClass as string;
+    }
+    if (valueIsValid(titleText))
+    {
+        conArea.title = titleText as string;
     }
     conArea.step = "1";
 
     return conArea;
 }
 
-function createTextInput(input:{defaultValue?:string, elementName?:string, elementID?:string, elementClass?:string})
+function createTextInput({
+    defaultValue, elementName, elementID, elementClass, titleText, maxLength
+}:{defaultValue?:string, elementName?:string, elementID?:string, elementClass?:string, titleText?:string, maxLength?:number
+})
 {
     let textInput = document.createElement("input");
 
     textInput.type = "text";
 
-    if (valueIsValid(input.defaultValue))
+    if (valueIsValid(defaultValue))
     {
-        textInput.value = input.defaultValue as string;
+        textInput.value = defaultValue as string;
     }
-    if (valueIsValid(input.elementName))
+    if (valueIsValid(elementName))
     {
-        textInput.name = input.elementName as string;
+        textInput.name = elementName as string;
     }
-    if (valueIsValid(input.elementID))
+    if (valueIsValid(elementID))
     {
-        textInput.id = input.elementID as string;
+        textInput.id = elementID as string;
     }
-    if (valueIsValid(input.elementClass))
+    if (valueIsValid(elementClass))
     {
-        textInput.className = input.elementClass as string;
+        textInput.className = elementClass as string;
+    }
+    if (valueIsValid(titleText))
+    {
+        textInput.title = titleText as string;
+    }
+    if (valueIsValid(maxLength))
+    {
+        textInput.maxLength = maxLength as number;
     }
     return textInput;
 }
 
-function createCheckbox(input:{value?:string, isActive:boolean, elementName?:string, elementClass?:string})
+function createCheckbox({
+    value, isActive=false, elementName, elementId, elementClass, titleText
+}:{
+    value?:string, isActive?:boolean, elementId?:string, elementName?:string, elementClass?:string, titleText?:string
+    })
 {
     let ckbox = document.createElement("input");
 
     ckbox.type = "checkbox";
 
-    if (valueIsValid(input.value))
+    if (valueIsValid(value))
     {
-        ckbox.value = input.value as string;
+        ckbox.value = value as string;
     }
-    if (valueIsValid(input.elementName))
+    if (valueIsValid(elementId))
     {
-        ckbox.name = input.elementName as string;
+        ckbox.id = elementId as string;
     }
-    if (valueIsValid(input.elementClass))
+    if (valueIsValid(elementName))
     {
-        ckbox.className = input.elementClass as string;
+        ckbox.name = elementName as string;
     }
-    ckbox.checked = input.isActive;
+    if (valueIsValid(elementClass))
+    {
+        ckbox.className = elementClass as string;
+    }
+    if (valueIsValid(titleText))
+    {
+        ckbox.title = titleText as string;
+    }
+    ckbox.checked = isActive;
 
     return ckbox;
 }
 
-function createSelectOption(value:string, text:string)
+function createSelect({elementName, elementID, elementClass, titleText}:{elementName?:string, elementID?:string, elementClass?:string, titleText?:string})
+{
+    let select = document.createElement("select");
+
+    if (valueIsValid(elementID))
+    {
+        select.id = elementID as string;
+    }
+    if (valueIsValid(elementName))
+    {
+        select.name = elementName as string;
+    }
+    if (valueIsValid(elementClass))
+    {
+        select.className = elementClass as string;
+    }
+    if (valueIsValid(titleText))
+    {
+        select.title = titleText as string;
+    }
+    return select;
+}
+
+function createSelectOption({value, text}:{value:string, text:string})
 {
     let blunt = document.createElement("option");
 
@@ -144,16 +227,79 @@ function createSelectOption(value:string, text:string)
     return blunt;
 }
 
-function enableOrDisableElements(input:{disable:boolean, className:string}) {
+function addOptionToSelect({value, text, select}:{value:string, text:string, select:HTMLSelectElement}) 
+{
+    let blunt = document.createElement("option");
 
-    let ranged = <HTMLCollection><any> document.getElementsByClassName(input.className);
+    blunt.value = value;
+
+    blunt.innerText = text;
+    
+    select.appendChild(blunt);
+}
+
+function createFieldset({fieldsetId, legendText}:{fieldsetId:string, legendText?:string})
+{
+    let fieldset = document.createElement("fieldset");
+
+    fieldset.id = fieldsetId;
+
+    if (valueIsValid(legendText))
+    {
+        let legend = document.createElement("legend");
+
+        legend.innerText = legendText as string;
+
+        fieldset.appendChild(legend);
+    }
+    return fieldset;
+}
+
+function createLabel({targetId, labelText}:{targetId:string, labelText:string})
+{
+    let label = document.createElement("label");
+
+    label.htmlFor = targetId;
+
+    label.innerText = labelText;
+
+    return label;
+}
+
+function createParagraph({
+    elementId, elementsToAppend, elementToAppendParagraphTo
+}:{
+    elementId?:string, elementsToAppend?:HTMLElement[], elementToAppendParagraphTo?:HTMLElement})
+{
+    let p = document.createElement("p");
+
+    if (valueIsValid(elementId))
+    {
+        p.id = elementId as string;
+    }
+    if (valueIsValid(elementsToAppend))
+    {
+        elementsToAppend?.forEach((element) => {
+            p.appendChild(element);
+        })
+    }
+    if (valueIsValid(elementToAppendParagraphTo))
+    {
+        elementToAppendParagraphTo?.appendChild(p);
+    }
+    return p;
+}
+
+function enableOrDisableElements({disable, className}:{disable:boolean, className:string}) 
+{
+    let ranged = <HTMLCollection><any> document.getElementsByClassName(className);
 
     let ranged2 = <HTMLInputElement[]> Array.from(ranged);
 
     console.log(ranged2);
 
     ranged2.forEach(element => {
-        element.disabled = input.disable
+        element.disabled = disable;
     });
 }
 
@@ -161,16 +307,24 @@ function hideOrUnhideElements({hideClassNames, unhideClassNames}:{hideClassNames
 {
     hideClassNames.forEach(element => {
         
-        let ele = <HTMLElement><any> document.getElementsByClassName(element);
+        let ele = <HTMLCollection><any> document.getElementsByClassName(element);
 
-        ele.hidden = true;
+        let elea = Array.from(ele) as HTMLElement[];
+
+        elea.forEach((e)=>{
+            e.hidden = true;
+        });
     });
     unhideClassNames.forEach(element => {
         
-        let ele = <HTMLElement><any> document.getElementsByClassName(element);
+        let ele = <HTMLCollection><any> document.getElementsByClassName(element);
 
-        ele.hidden = false;
-    })
+        let elea = Array.from(ele) as HTMLElement[];
+
+        elea.forEach((e)=>{
+            e.hidden = true;
+        });
+    });
 }
 
 function getInputElementValue(inputId:string) 
@@ -187,32 +341,60 @@ function getSelectElementValue(inputId:string)
     return input.value;
 }
 
-function getSingleInput({inputId, numberOfTabObjects, ignoreIfDisabled=true}:{
-    inputId:string, numberOfTabObjects:number, ignoreIfDisabled?:boolean})
+function getSingleInput({inputId, numberOfTabObjects, ignoreIfDisabled=true, ignoreIfBlank, prefixType=PrefixValueType.id}:{
+    inputId:string, numberOfTabObjects:number, ignoreIfDisabled?:boolean, ignoreIfBlank:boolean, prefixType?:PrefixValueType})
 {
     let input_ = <HTMLInputElement><any> document.getElementById(inputId);
 
-    if (!ignoreIfDisabled || !input_.disabled)
+    if ((!ignoreIfDisabled || !input_.disabled) && (!ignoreIfBlank || input_.value !== ""))
     {
-        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", inputId, ":", input_.value, "]\n" );
+        let prefix;
+
+        if (prefixType === PrefixValueType.id)
+        {
+            prefix = input_.id;
+        }
+        else 
+        {
+            prefix = (prefixType === PrefixValueType.name ? input_.name : input_.className);
+        }
+        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", prefix, ":", input_.value, "]\n" );
     }
     return input_;
 }
 
-function getSingleCheckBox({inputId, numberOfTabObjects, ignoreIfDisabled=true}:{inputId:string, numberOfTabObjects:number, ignoreIfDisabled?:boolean})
+function getSingleCheckBox({
+    inputId, numberOfTabObjects, ignoreIfDisabled=true, prefixType=PrefixValueType.id
+}:{
+    inputId:string, numberOfTabObjects:number, ignoreIfDisabled?:boolean, prefixType?:PrefixValueType})
 {
     let input_ = <HTMLInputElement><any> document.getElementById(inputId);
 
     if ((!ignoreIfDisabled || !input_.disabled) && input_.checked)
     {
-        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", inputId, ":", input_.value, "]\n" );
+        let prefix;
+
+        if (prefixType === PrefixValueType.id)
+        {
+            prefix = input_.id;
+        }
+        else if (prefixType === PrefixValueType.value)
+        {
+            prefix = input_.value;
+        }
+        else 
+        {
+            prefix = (prefixType === PrefixValueType.name ? input_.name : input_.className);
+        }
+        pushObject.pushTo.push(pushObject.tabObject.repeat(numberOfTabObjects), "[", prefix, "]\n" );
     }
     return input_.checked;
 }
 
-function getMultipleInputs(
-    {inputIds, inputClass, useClassInPlaceOfId=false, appendClassInFrontOfId=false, 
-        numberOfTabObjects, ignoreIfDisabled=false}:{inputIds?:string[], inputClass?:string, 
+function getMultipleInputs({
+    inputIds, inputClass, useClassInPlaceOfId=false, appendClassInFrontOfId=false, 
+        numberOfTabObjects, ignoreIfDisabled=false
+}:{inputIds?:string[], inputClass?:string, 
         useClassInPlaceOfId?:boolean, appendClassInFrontOfId?:boolean, 
         numberOfTabObjects:number, ignoreIfDisabled?:boolean})
 {
@@ -237,9 +419,10 @@ function getMultipleInputs(
             throw new ReferenceError("The value for input.inputIds is undefined");
         }
         inputIds.forEach(element => {
-            classElements.push(
-                document.getElementById(element) as HTMLInputElement
-            );
+
+            let el = document.getElementById(element) as HTMLInputElement;
+
+            classElements.push(el);
         });
     }
     if (ignoreIfDisabled)
@@ -338,11 +521,11 @@ function isCheckboxChecked(inputId:string)
     return input_.checked;
 }
 
-function uncheckCheckbox(input:{uncheckTarget:boolean, targetId:string})
+function uncheckCheckbox({uncheckTarget, targetId}:{uncheckTarget:boolean, targetId:string})
 {
-    if (input.uncheckTarget)
+    if (uncheckTarget)
     {
-        let target = <HTMLInputElement><any> document.getElementById(input.targetId);
+        let target = <HTMLInputElement><any> document.getElementById(targetId);
         target.checked = false;
     }
 }
